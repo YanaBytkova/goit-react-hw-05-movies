1dec2d1dda0fec47b8578a0be9c54e3edf7f3d06
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-11-16 21:33:41 +0200

    fix deploy

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   //   </BrowserRouter>
   // </ModalContextProvider>
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
-    // <BrowserRouter>
+    {/* <BrowserRouter basename="/goit-react-hw-05-movies"> */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
